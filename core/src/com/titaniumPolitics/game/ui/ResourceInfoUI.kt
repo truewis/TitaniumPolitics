4506020ef6218046c9aaf5c78e076e76012c35f8
Jinseo Park <parkjinseo2@naver.com>
2024-01-27 18:17:59 -0600

    feat: research information ui expanded

--- core/src/com/titaniumPolitics/game/ui/ResourceInfoUI.kt
+++ core/src/com/titaniumPolitics/game/ui/ResourceInfoUI.kt
@@ -15,6 +15,7 @@ class ResourceInfoUI(skin: Skin?, var gameState: GameState) : Table(skin), KTabl
 {
     init
     {
+        debug()
 
 // Assuming you have an instance of Information like this
         val information = Information(
@@ -23,30 +24,318 @@ class ResourceInfoUI(skin: Skin?, var gameState: GameState) : Table(skin), KTabl
             type = "resources",
             tgtTime = 0,
             tgtPlace = "Place",
-            tgtResource = "Resource",
+            tgtResource = "ration",
             amount = 10
         ).also { it.knownTo.add("Author"); it.credibility = 100 }
 
         val dataTable = scene2d.table {
-            label("Resource") { setAlignment(Align.center) }
-            label(information.tgtResource) { setAlignment(Align.center) }
+            label("Resource") {
+                it.growX()
+                setAlignment(Align.center)
+            }
+            label("Amount") {
+                it.growX()
+                setAlignment(Align.center)
+            }
             row()
-            label("Amount") { setAlignment(Align.center) }
-            label(information.amount.toString()) { setAlignment(Align.center) }
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            row()
+
+            label(information.tgtResource) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+            label(information.amount.toString()) {
+                it.fill()
+                setAlignment(Align.center)
+            }
+
             setFillParent(true)
         }
 
 
         val authorLabel = label("Author: ${information.author}") { setAlignment(Align.center) }
+        row()
         val creationTimeLabel = label("Creation Time: ${information.creationTime}") { setAlignment(Align.center) }
+        row()
         val typeLabel = label("Type: ${information.type}") { setAlignment(Align.center) }
+        row()
         val tgtTimeLabel = label("Target Time: ${information.tgtTime}") { setAlignment(Align.center) }
+        row()
         val tgtPlaceLabel = label("Target Place: ${information.tgtPlace}") { setAlignment(Align.center) }
         row()
         val scrollPane = scrollPane {
+            it.grow()
             addActor(dataTable)
-            setFillParent(true)
-            setScrollingDisabled(false, false)
+            setScrollingDisabled(true, false)
         }
 
 
