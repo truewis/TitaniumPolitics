6a5d1557249c12154fe1c8f3346ab4ce9aa3446e
truewis <truewis@uchicago.edu>
2025-01-19 09:23:45 -0600

    feat: added observer events

---
+++ core/src/com/titaniumPolitics/game/events/Event_ObserverIntroAfterMeeting2.kt
@@ -0,0 +1,38 @@
+package com.titaniumPolitics.game.events
+
+import com.titaniumPolitics.game.core.GameState
+import com.titaniumPolitics.game.ui.DialogueUI
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
+
+@Serializable
+class Event_ObserverIntroAfterMeeting2 : EventObject("Mysterious orders from the Observer.", true)
+{
+    //Infrastructure Division Leader gives a speech. Quest is completed when the game starts.
+    override fun injectParent(gameState: GameState)
+    {
+        super.injectParent(gameState)
+        //Injected at the start of the game. No action required.
+
+    }
+
+    @Transient
+    val func = { _: Int , _: Int ->
+        if(parent.player.place.name=="spacePort"&&parent.player.currentMeeting?.currentCharacters?.contains("observer")==true) {
+            DialogueUI.instance.playDialogue("ObserverIntroAfterMeeting2")
+            deactivate()
+        }
+    }
+
+    override fun activate()
+    {
+        //Play dialogue right after the meeting
+        //TODO: check if the player has followed the orders.
+        parent.timeChanged += func
+    }
+
+    override fun deactivate()
+    {
+        parent.timeChanged -= func
+    }
+}
