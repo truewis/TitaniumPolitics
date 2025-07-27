19d0e205b6371c5a58098cdef819b7b82431cdd8
Jinseo Park <jinseop@stanford.edu>
2025-07-27 08:16:11 -0700

    fix: reimplemented Cabinet mark

--- core/src/com/titaniumPolitics/game/ui/AssistantUI.kt
+++ core/src/com/titaniumPolitics/game/ui/AssistantUI.kt
@@ -4,227 +4,78 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.Group
-import com.badlogic.gdx.scenes.scene2d.Touchable
-import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Table
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
-import com.badlogic.gdx.utils.Align
 import com.titaniumPolitics.game.core.GameState
 import com.titaniumPolitics.game.core.ReadOnly
 import com.titaniumPolitics.game.ui.map.MapUI
-import com.titaniumPolitics.game.ui.map.PlaceSelectionUI
-import com.titaniumPolitics.game.ui.widget.CabinetWindowUI
-import com.titaniumPolitics.game.ui.widget.CharacterSelectUI
-import ktx.scene2d.*
+import com.titaniumPolitics.game.ui.widget.CabinetWindowContainerUI
 import ktx.scene2d.Scene2DSkin.defaultSkin
 
 class AssistantUI(gameState: GameState) : Table(defaultSkin) {
 
-    val cabinetWindowUIs = mutableListOf<CabinetWindowUI>()
+    val cabinetWindowUIs = mutableListOf<CabinetWindowContainerUI>()
 
     init {
         val buttonWidth = 180f
         val buttonHeight = 540f
-        val buttonGap = 70f
+        val buttonYGap = 70f
         val buttonXGap = -7f
         padLeft(-20f)
 
-        val MapButton = scene2d.table {
-            name = "MapButton"
-            stack {
-                it.fill()
-                container(
-                    image("cabinetHandleLight2") {
-                    }) {
-                    size(buttonWidth, buttonHeight)
-                    setTouchable(Touchable.disabled)//Want to touch the text, through the image.
-                }
-            }
-            val text = scene2d.container(
-                label("MAP", "docTitle") {
-                    setFontScale(0.4f)
-                    color = Color.DARK_GRAY
-                    setAlignment(Align.left)
-                }
-            ) {
-                fill()
-                size(100f, 30f)
-            }
-            addActor(text)
-            text.setPosition(160f, 140f)
-            text.isTransform = true // Enable transformations for the text actor
-            text.rotateBy(90f)
-            val map =  MapUI(gameState)
-            val UI = CabinetWindowUI(this@table, map, 0f, {map.refresh()})
-            addActor(UI)
-            UI.setSize(CapsuleStage.instance.width, CapsuleStage.instance.height)
-            UI.setPosition(-CapsuleStage.instance.width, 0f)
-            UI.layout()
-            cabinetWindowUIs.add(UI) // Add the MapUI to the list of CabinetWindowUIs
+        val mapUI = MapUI(gameState)
+        val mapButton = CabinetWindowContainerUI(
+            title = "MAP",
+            content = mapUI,
+            xOffset = buttonXGap,
+            yOffset = 0f,
+            openAction = { mapUI.refresh() }
+        )
+        addActor(mapButton)
+        cabinetWindowUIs.add(mapButton)
+        mapButton.setSize(buttonWidth, buttonHeight + 10f)
+        mapButton.setPosition(0f, 0f)
 
-            text.addListener(object : ClickListener() {
-                override fun clicked(event: com.badlogic.gdx.scenes.scene2d.InputEvent?, x: Float, y: Float) {
-
-                    //Open Map UI
-                    UI.changeOpenState(!UI.isOpen)
-                }
-            }
+        val infoUI = InformationViewUI(gameState)
+        val informationButton =
+            CabinetWindowContainerUI(
+                title = "INFORMATION",
+                content = infoUI,
+                xOffset = buttonXGap,
+                yOffset = buttonYGap,
+                openAction = { infoUI.refresh("creationTime") }
             )
-        }
-        addActor(MapButton)
-        MapButton.setSize(buttonWidth, buttonHeight + 10f)
-        MapButton.setPosition(0f, 0f)
+        addActor(informationButton)
+        cabinetWindowUIs.add(informationButton)
+        informationButton.setSize(buttonWidth, buttonHeight + 10f)
+        informationButton.setPosition(buttonXGap, buttonYGap)
 
-        val InformationButton = scene2d.table {
-            name = "InformationButton"
-            stack {
-                it.fill()
-                container(
-                    image("cabinetHandleLight2") {
-                    }) {
-                    size(buttonWidth, buttonHeight)
-                    setTouchable(Touchable.disabled)
-                }
-            }
-            val text = scene2d.container(
-                label("INFORMATION", "docTitle") {
-                    setFontScale(0.4f)
-                    color = Color.DARK_GRAY
-                    setAlignment(Align.left)
-                }
-            ) {
-                fill()
-                size(100f, 30f)
-            }
-            addActor(text)
-            text.setPosition(160f, 140f)
-            text.isTransform = true // Enable transformations for the text actor
-            text.rotateBy(90f)
-            val infoUI = InformationViewUI(gameState)
-            val UI = CabinetWindowUI(this@table, infoUI, buttonXGap, {infoUI.refresh("name")})
-            addActor(UI)
-            UI.setSize(CapsuleStage.instance.width, CapsuleStage.instance.height)
-            UI.setPosition(-CapsuleStage.instance.width, -buttonGap)
-            UI.layout()
-            cabinetWindowUIs.add(UI) // Add the MapUI to the list of CabinetWindowUIs
-
-            text.addListener(object : ClickListener() {
-                override fun clicked(event: com.badlogic.gdx.scenes.scene2d.InputEvent?, x: Float, y: Float) {
-
-                    //Open Map UI
-                    UI.changeOpenState(!UI.isOpen)
-                }
-            }
+        val calendarUI = CalendarUI(gameState)
+        val calendarButton =
+            CabinetWindowContainerUI(
+                title = "CALENDAR",
+                content = calendarUI,
+                xOffset = 2 * buttonXGap,
+                yOffset = 2 * buttonYGap,
+                openAction = { calendarUI.refresh() }
             )
-        }
-        addActor(InformationButton)
-        InformationButton.setSize(buttonWidth, buttonHeight + 10f)
-        InformationButton.setPosition(buttonXGap, buttonGap)
-        val calendarLabel = Label("0", defaultSkin, "trnsprtConsole").also {
-            it.setFontScale(2f)
-            it.color = Color.BLACK
-            it.setAlignment(Align.center, Align.center)
-        }
-        val CalendarButton = scene2d.table {
-            name = "CalendarButton"
-            stack {
-                it.fill()
-                container(
-                    image("cabinetHandleLight2") {
-                    }) {
-                    size(buttonWidth, buttonHeight)
-                    setTouchable(Touchable.disabled)
-                }
-            }
-            val text = scene2d.container(
-                label("CALENDAR", "docTitle") {
-                    setFontScale(0.4f)
-                    color = Color.DARK_GRAY
-                    setAlignment(Align.left)
-                }
-            ) {
-                fill()
-                size(100f, 30f)
-            }
-            addActor(text)
-            text.setPosition(160f, 140f)
-            text.isTransform = true // Enable transformations for the text actor
-            text.rotateBy(90f)
-            val calUI = CalendarUI(gameState)
-            val UI = CabinetWindowUI(this@table, calUI, 2 * buttonXGap, {calUI.refresh()})
-            addActor(UI)
-            UI.setSize(CapsuleStage.instance.width, CapsuleStage.instance.height)
-            UI.setPosition(-CapsuleStage.instance.width, -2 * buttonGap)
-            UI.layout()
-            cabinetWindowUIs.add(UI) // Add the MapUI to the list of CabinetWindowUIs
+        addActor(calendarButton)
+        cabinetWindowUIs.add(calendarButton)
+        calendarButton.setSize(buttonWidth, buttonHeight + 10f)
+        calendarButton.setPosition(2 * buttonXGap, 2 * buttonYGap)
 
-            text.addListener(object : ClickListener() {
-                override fun clicked(event: com.badlogic.gdx.scenes.scene2d.InputEvent?, x: Float, y: Float) {
-
-                    //Open Map UI
-                    UI.changeOpenState(!UI.isOpen)
-                }
-            }
-            )
-        }
-        addActor(CalendarButton)
-        CalendarButton.setSize(buttonWidth, buttonHeight + 10f)
-        CalendarButton.setPosition(2 * buttonXGap, 2 * buttonGap)
-
-        val politiciansInfoButton = scene2d.table {
-            name = "politiciansInfoButton"
-            stack {
-                it.fill()
-                container(
-                    image("cabinetHandleLight2") {
-                    }) {
-                    size(buttonWidth, buttonHeight)
-                    setTouchable(Touchable.disabled)
-                }
-
-            }
-            val text = scene2d.container(
-                label("POLITICS", "docTitle") {
-                    setFontScale(0.4f)
-                    color = Color.DARK_GRAY
-                    setAlignment(Align.left)
-                }
-            ) {
-                fill()
-                size(100f, 30f)
-            }
-            addActor(text)
-            text.setPosition(160f, 140f)
-            text.isTransform = true // Enable transformations for the text actor
-            text.rotateBy(90f)
-            val polUI = PoliticiansInfoUI(gameState)
-            val UI = CabinetWindowUI(this@table, polUI, 3 * buttonXGap, {polUI.refresh()})
-            addActor(UI)
-            UI.setSize(CapsuleStage.instance.width, CapsuleStage.instance.height)
-            UI.setPosition(-CapsuleStage.instance.width, -3 * buttonGap)
-            UI.layout()
-            cabinetWindowUIs.add(UI) // Add the MapUI to the list of CabinetWindowUIs
-
-            text.addListener(object : ClickListener() {
-                override fun clicked(event: com.badlogic.gdx.scenes.scene2d.InputEvent?, x: Float, y: Float) {
-
-                    //Open Map UI
-                    UI.changeOpenState(!UI.isOpen)
-                }
-            }
-            )
-        }
+        val politiciansUI = PoliticiansInfoUI(gameState)
+        val politiciansInfoButton = CabinetWindowContainerUI(
+            title = "POLITICS",
+            content = politiciansUI,
+            xOffset = 3 * buttonXGap,
+            yOffset = 3 * buttonYGap,
+            openAction = { politiciansUI.refresh() }
+        )
         addActor(politiciansInfoButton)
+        cabinetWindowUIs.add(politiciansInfoButton)
         politiciansInfoButton.setSize(buttonWidth, buttonHeight + 10f)
-        politiciansInfoButton.setPosition(3 * buttonXGap, 3 * buttonGap)
+        politiciansInfoButton.setPosition(3 * buttonXGap, 3 * buttonYGap)
 
-        gameState.timeChanged += { _, y ->
-            Gdx.app.postRunnable {
-
-                calendarLabel.setText((gameState.day).toString()) // Current Date.
-
-            }
-        }
 
         //Mark the calendar button When new meeting is scheduled within the next 5 days.
         //Also check CalendarUI
@@ -234,58 +85,12 @@ class AssistantUI(gameState: GameState) : Table(defaultSkin) {
                         gameState.playerName
                     )
                 ) {//5 days from today's start
-                    markButton(findActor("CalendarButton"))
+                    calendarButton.changeMarkedState(true)
                 }
             }
         }
     }
 
-    fun changeColorRecursively(actor: Actor, color: Color) {
-        actor.color = color
-        if (actor is Group) {
-            actor.children.forEach { child ->
-                changeColorRecursively(child, color)
-            }
-        }
-    }
-
-    //Change the color to green, display green marker, and blink the button.
-    fun markButton(actor: Group) {
-
-        if (actor.findActor<Actor>("GreenMarker_${actor.name}") != null)
-            return //If the marker already exists, do not mark again.
-        changeColorRecursively(actor, Color.GREEN)
-        actor.children.forEach { child ->
-            child.color = Color.GREEN
-        }
-
-        actor.addAction(
-            com.badlogic.gdx.scenes.scene2d.actions.Actions.forever(
-                com.badlogic.gdx.scenes.scene2d.actions.Actions.sequence(
-                    com.badlogic.gdx.scenes.scene2d.actions.Actions.alpha(0f, 0.5f),
-                    com.badlogic.gdx.scenes.scene2d.actions.Actions.alpha(1f, 0.5f)
-                )
-            )
-        )
-        val marker = scene2d.image("BadgeRound") {
-            name = "GreenMarker_${actor.name}"
-            color = Color.GREEN
-            setSize(25f, 25f)
-            setPosition(actor.width * 0.75f - width / 2, actor.height * 0.75f - height / 2)
-        }
-        actor.addActor(marker) //Add the marker to the actor.
-    }
-
-    //unmark the button, change the color to white.
-    fun unmarkButton(actor: Group) {
-        changeColorRecursively(actor, Color.WHITE)
-        actor.children.forEach { child ->
-            child.color = Color.WHITE
-        }
-        actor.clearActions()
-        actor.findActor<Actor>("GreenMarker_${actor.name}")?.remove() //Remove the marker if it exists.
-
-    }
 
 
 }
---
+++ core/src/com/titaniumPolitics/game/ui/widget/CabinetWindowContainerUI.kt
@@ -0,0 +1,174 @@
+package com.titaniumPolitics.game.ui.widget
+
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.scenes.scene2d.Actor
+import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.Touchable
+import com.badlogic.gdx.scenes.scene2d.actions.Actions
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import com.badlogic.gdx.utils.Align
+import com.titaniumPolitics.game.ui.AssistantUI
+import com.titaniumPolitics.game.ui.CapsuleStage
+import ktx.scene2d.KTable
+import ktx.scene2d.Scene2DSkin
+import ktx.scene2d.button
+import ktx.scene2d.container
+import ktx.scene2d.image
+import ktx.scene2d.label
+import ktx.scene2d.scene2d
+import ktx.scene2d.stack
+import ktx.scene2d.table
+
+open class CabinetWindowContainerUI(val title: String, val content: Actor, val xOffset: Float, val yOffset: Float, val openAction : () -> Unit = {}) :
+    Table(Scene2DSkin.defaultSkin), KTable {
+    val onClose = ArrayList<() -> Unit>()
+    var isOpen = false
+        private set
+
+    val buttonWidth = 180f
+    val buttonHeight = 540f
+    val titleLabel: Label
+    init {
+        name = title
+        stack {
+            it.fill()
+            container(
+                image("cabinetHandleLight2") {
+                }) {
+                size(this@CabinetWindowContainerUI.buttonWidth, this@CabinetWindowContainerUI.buttonHeight)
+                setTouchable(Touchable.disabled)//Want to touch the text, through the image.
+            }
+        }
+        val text = scene2d.container(
+            label(title, "docTitle") {
+                setFontScale(0.4f)
+                color = Color.DARK_GRAY
+                setAlignment(Align.left)
+            }
+        ) {
+            fill()
+            size(100f, 30f)
+        }
+        addActor(text)
+        titleLabel = text.actor as Label
+        text.setPosition(160f, 140f)
+        text.isTransform = true // Enable transformations for the text actor
+        text.rotateBy(90f)
+        val UI = scene2d.stack {
+            image("GradientBottom") {
+                color = Color.BLACK
+            }
+            image("BackgroundNoiseHD")
+            table {
+                add().growX()
+                button {
+                    it.fill()
+                    it.size(70f)
+                    image("XGrunge")
+                    addListener(object : ClickListener() {
+                        override fun clicked(event: InputEvent?, x: Float, y: Float) {
+                            this@CabinetWindowContainerUI.onClose.forEach { it() }
+                            this@CabinetWindowContainerUI.changeOpenState(false)
+                        }
+                    })
+                }
+
+                row()
+                add(this@CabinetWindowContainerUI.content).colspan(2).grow()
+            }
+        }
+        addActor(UI)
+        UI.setSize(CapsuleStage.instance.width, CapsuleStage.instance.height)
+        UI.setPosition(-CapsuleStage.instance.width, -yOffset)
+        UI.layout()
+
+        text.addListener(object : ClickListener() {
+            override fun clicked(event: com.badlogic.gdx.scenes.scene2d.InputEvent?, x: Float, y: Float) {
+
+                //Open content UI
+                changeOpenState(true)
+                changeMarkedState(false)
+            }
+        }
+        )
+
+
+    }
+
+    fun changeOpenState(open: Boolean) {
+        isOpen = open
+        val otherCabinets = (parent as AssistantUI).cabinetWindowUIs
+            .filter { it != this } // Exclude the current cabinet
+        //Add actions animations accordingly
+        actions.clear()
+        otherCabinets.forEach { it.actions.clear() } // Close other cabinets
+        if (open) {
+            openAction() // Call the open action if provided
+            addAction(
+                Actions.moveTo(
+                    stage.width, //When open, move to the right side of the screen. It should not depend on the parent actor's x offset.
+                    y,
+                    0.5f
+                )
+            )
+            otherCabinets.forEach {
+                it.addAction(
+                    Actions.moveTo(
+                        -200f, //When closed, move to the left side of the screen to hide the handle. It should not depend on the parent actor's x offset.
+                        it.y,
+                        0.5f
+                    )
+                )
+            } // Close other cabinets
+        } else {
+            addAction(
+                Actions.moveTo(
+                    xOffset,
+                    y,
+                    0.5f
+                )
+            )
+            otherCabinets.forEach {
+                it.addAction(
+                    Actions.moveTo(
+                        it.xOffset, //When closed, move to the left side of the screen to hide the handle. It should not depend on the parent actor's x offset.
+                        it.y,
+                        0.5f
+                    )
+                )
+            }
+        }
+    }
+
+    fun changeMarkedState(marked: Boolean) {
+        if (marked) {
+            titleLabel.color = Color.GREEN
+            val marker = scene2d.image("BadgeRound") {
+                name = "GreenMarker"
+                color = Color.GREEN
+                setSize(25f, 25f)
+
+            }
+            marker.addAction(
+                Actions.forever(
+                    Actions.sequence(
+                        Actions.delay(0.5f),
+                        Actions.alpha(0f, 0.2f),
+                        Actions.alpha(1f, 0.2f)
+                    )
+                )
+            )
+            addActor(marker) //Add the marker to the actor.
+            marker.setPosition(120f, 90f)
+        } else {
+            titleLabel.color = Color.DARK_GRAY
+            val marker = findActor<Image>("GreenMarker")
+            marker?.remove()
+        }
+    }
+
+
+}
