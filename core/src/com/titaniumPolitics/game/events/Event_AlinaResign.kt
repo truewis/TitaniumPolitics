294bae196994eba0030e886e04c8fca178047e8e
Jinseo Park <jinseop@stanford.edu>
2025-08-13 07:13:31 -0700

    fix: fixed event condition

--- core/src/com/titaniumPolitics/game/events/Event_AlinaResign.kt
+++ core/src/com/titaniumPolitics/game/events/Event_AlinaResign.kt
@@ -6,7 +6,7 @@ import kotlinx.serialization.Serializable
 class Event_AlinaResign : EventObject("Introduction of Alina.", true) {
 
     override fun exec(a: Int, b: Int) {
-        if (b > 96 && parent.player.currentMeeting != null && parent.parties["infrastructure"]!!.leader == "Alina" && parent.player.currentMeeting!!.currentCharacters.containsAll(
+        if (parent.day > 3 && parent.player.currentMeeting != null && parent.parties["infrastructure"]!!.leader == "Alina" && parent.player.currentMeeting!!.currentCharacters.containsAll(
                 listOf("Alina")
             )
         ) {
