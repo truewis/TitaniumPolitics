86b94b01e823db36c389cd2f86ea5b0b53f08bc8
Jinseo Park <jinseop@stanford.edu>
2025-08-29 22:07:59 -0700

    balance: talk mutuality decrease condition

--- core/src/com/titaniumPolitics/game/core/gameActions/Talk.kt
+++ core/src/com/titaniumPolitics/game/core/gameActions/Talk.kt
@@ -55,11 +55,11 @@ data class Talk(
             super.execute()
         }
         //The person's mutuality toward the subject character decreases.
-        //But only if who doesn't like the subject character, or the subject character is not the boss of who.
+        //But only if who doesn't like the subject character and the subject character is not the boss of who.
         if (parent.getMutNorm(
                 who,
                 sbjCharacter
-            ) < 0 || parent.parties.none { who in it.value.members && it.value.leader == sbjCharacter }
+            ) < 0 && parent.parties.none { who in it.value.members && it.value.leader == sbjCharacter }
         )
             parent.setMutuality(
                 who,
