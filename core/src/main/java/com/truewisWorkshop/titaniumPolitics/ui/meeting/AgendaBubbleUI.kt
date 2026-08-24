2bc58f3050b3150bfa749a361ab5d79f926a1e68
Jinseo Park <jinseop@stanford.edu>
2026-08-23 21:16:11 -0700

    feat: add persuasiveness meter to agenda bubble UI

--- core/src/main/java/com/truewisWorkshop/titaniumPolitics/ui/meeting/AgendaBubbleUI.kt
+++ core/src/main/java/com/truewisWorkshop/titaniumPolitics/ui/meeting/AgendaBubbleUI.kt
@@ -1,10 +1,14 @@
 package com.titaniumPolitics.game.ui.meeting
 
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.utils.Align
 import com.titaniumPolitics.game.core.AgendaType
 import com.titaniumPolitics.game.core.MeetingAgenda
+import com.titaniumPolitics.game.core.ReadOnly
+import com.titaniumPolitics.game.ui.widget.MeterUI
 import ktx.scene2d.KTable
+import ktx.scene2d.container
 import ktx.scene2d.image
 import ktx.scene2d.label
 import ktx.scene2d.stack
@@ -15,6 +19,16 @@ class AgendaBubbleUI(val agenda: MeetingAgenda) : Table(), KTable {
         addListener(AgendaTooltipUI(agenda))
 
         with(agenda) {
+            val persuasivenessMin = ReadOnly.const("AgendaPersuasivenessMin")
+            val persuasivenessMax = ReadOnly.const("AgendaPersuasivenessMax")
+            val persuasivenessFill =
+                ((persuasiveness - persuasivenessMin) / (persuasivenessMax - persuasivenessMin)).toFloat().coerceIn(0f, 1f)
+            val persuasivenessBarColor = when {
+                persuasiveness < 0 -> Color(0.9f, 0.25f, 0.25f, 1f)
+                persuasiveness >= persuasivenessMax -> Color(0.25f, 0.85f, 0.3f, 1f)
+                else -> Color(0.95f, 0.8f, 0.2f, 1f)
+            }
+
             stack {
                 it.size(80f, 80f).fill()
                 image("BubbleShade") {
@@ -75,9 +89,21 @@ class AgendaBubbleUI(val agenda: MeetingAgenda) : Table(), KTable {
 
                     }
                 }
-                label(this@AgendaBubbleUI.agenda.persuasiveness.toInt().toString(), "docTitle") {
-                    setFontScale(0.18f)
-                    setAlignment(Align.bottomRight)
+                container(stack {
+                    container(MeterUI().apply {
+                        setValue(persuasivenessFill)
+                        color = persuasivenessBarColor
+                    }) {
+                        fill()
+                    }
+                    label(this@AgendaBubbleUI.agenda.persuasiveness.toInt().toString(), "docTitle") {
+                        setFontScale(0.16f)
+                        setAlignment(Align.center)
+                    }
+                }) {
+                    size(58f, 16f)
+                    padBottom(4f)
+                    align(Align.bottom)
                 }
             }
         }
